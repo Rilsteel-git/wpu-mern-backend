995b6624e38c833fbdc29af8a987b0bbaeb12512
Rilsteel-git <syahrill.ramadhan02@gmail.com>
2025-05-27 14:21:10 +0700

    Feat: server checking running

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,11 @@ async function init() {
         const app = express();
         app.use(bodyParser.json());      
         const PORT = process.env.PORT || 3000;
-            
+
         app.get('/', (req, res) => {
-            res.send('Server is running');
+            res.status(200).json({
+                message: 'Server is running'
+            })
         });
 
         app.use('/api', router);
