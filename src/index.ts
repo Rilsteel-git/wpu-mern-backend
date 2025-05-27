5dc1ee6eae8a737a6054964bb6f48291492258fd
Rilsteel-git <syahrill.ramadhan02@gmail.com>
2025-05-27 14:17:12 +0700

    Feat: Server checking endpoint

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,11 @@ async function init() {
         const app = express();
         app.use(bodyParser.json());      
         const PORT = process.env.PORT || 3000;
+            
+        app.get('/', (req, res) => {
+            res.send('Server is running');
+        });
+
         app.use('/api', router);
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
